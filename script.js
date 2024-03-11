3b307777b77801bdce7b3ebea3c4ad5f36b122f9
Wartusss <162714870+Wartusss@users.noreply.github.com>
2024-03-11 03:20:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,304 +2,267 @@ const scheduleData = {
   "monday": [
     {
       "time": "13:05",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
+      "week": "all",
       "course": "",
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": "Some additional information for Monday"
+      "additionalInfo": "",
     },
     {
       "time": "14:40",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Лекція",
-      "title": "Новітня Історія України",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "Онлайн",
-      "additionalInfo": "https://meet.google.com/yao-yrwr-heg"
+      "week": "all",
+      "course": "language",
+      "title": "Турецька та китайська мови: базовий рівень",
+      "teacher": "Козоріз О. П. / Нікітюк Т. В.",
+      "location": "441 / 442 аудиторія",
+      "additionalInfo": "",
     },
     {
       "time": "16:20",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Лекція",
-      "title": "Історія політики країн Заходу щодо України",
-      "teacher": "Машевський О.П.",
-      "location": "Онлайн",
-      "additionalInfo": "https://knu-ua.zoom.us/j/88035974768?pwd=NWxnZll2YkxiWkowaTZ3Y0lqVTZhdz09"
+      "week": "all",
+      "course": "lection",
+      "title": "Нова історія країн Азії та Африки",
+      "teacher": "Шевченко Н. І.",
+      "location": "457 аудиторія",
+      "additionalInfo": "",
     },
     {
       "time": "17:55",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Лекція",
-      "title": "Історія раннього Нового часу",
-      "teacher": "Пількевич А. Л.",
-      "location": "Онлайн",
-      "additionalInfo": "https://us02web.zoom.us/j/71399386454?pwd=RitDZGxhUVpWcHFpRkVUMU9ER0J3QT09"
+      "week": "all",
+      "course": "seminar",
+      "title": "Нова історія країн Азії та Африки",
+      "teacher": "Шевченко Н. І.",
+      "location": "457 аудиторія",
+      "additionalInfo": "",
     },
-    // Other lesson objects for different time slots and weeks
   ],
   "tuesday": [
     {
       "time": "13:05",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
+      "week": "all", 
       "course": "",
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": "Some additional information for Monday"
+      "additionalInfo": "",
     },
     {
       "time": "14:40",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": ""
+      "week": "all",
+      "course": "seimnar",
+      "title": "Історія українського та зарубіжного китаєзнавства",
+      "teacher": "Урусов В. Б.",
+      "location": "460 аудиторія",
+      "additionalInfo": "",
     },
     {
       "time": "16:20",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Среміна",
-      "title": "Історія раннього Нового часу",
-      "teacher": "Грегуль А. Ю",
-      "location": "453 аудиторія",
-      "additionalInfo": ""
+      "week": "all",
+      "course": "language",
+      "title": "Турецька та китайська мови: базовий рівень",
+      "teacher": "Козоріз О. П. / Нікітюк Т. В.",
+      "location": "438 / 442 аудиторія",
+      "additionalInfo": "",
     },
     {
       "time": "17:55",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Семінар",
-      "title": "Історія Східної Європи та Росії",
-      "teacher": "Гуменний С. Л.",
-      "location": "438 аудиторія",
-      "additionalInfo": ""
+      "week": "upper",
+      "course": "lection",
+      "title": "Історія українського та зарубіжного китаєзнавства",
+      "teacher": "Урусов В. Б.",
+      "location": "460 аудиторія",
+      "additionalInfo": "",
     },
-    // Other lesson objects for different time slots and weeks
   ],
   "wednesday": [
     {
       "time": "13:05",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
+      "week": "all",
       "course": "",
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": ""
+      "additionalInfo": "",
     },
     {
       "time": "14:40",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Лекція",
-      "title": "Історія Східної Європи та Росії",
-      "teacher": "Мордвінцев В. М.",
-      "location": "Онлайн; 10:00",
-      "additionalInfo": "https://zoom.us/j/5169999552?pwd=UENVOXlJYm9hcWRQTDFnd3ZMemgzdz09"
+      "week": "all",
+      "course": "language",
+      "title": "Англійська",
+      "teacher": "Цупрун Т. В.",
+      "location": "440 аудиторія",
+      "additionalInfo": "",
     },
     {
       "time": "16:20",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Семінар",
-      "title": "Історія політичного устрою країн Західної Європи ",
-      "teacher": "Іванов О. Ф.",
-      "location": "451 аудиторія",
-      "additionalInfo": ""
+      "week": "all",
+      "course": "language",
+      "title": "Турецька та китайська мови: базовий рівень",
+      "teacher": "Козоріз О. П. / Нікітюк Т. В.",
+      "location": "346 / 435 аудиторія",
+      "additionalInfo": "",
     },
     {
       "time": "17:55",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": ""
+      "week": "upper",
+      "course": "language",
+      "title": "Турецька та китайська мови: базовий рівень",
+      "teacher": "Козоріз О. П. / Нікітюк Т. В.",
+      "location": "438 / 442 аудиторія",
+      "additionalInfo": "",
     },
-    // Other lesson objects for different time slots and weeks
   ],
   "thursday": [
     {
       "time": "13:05",
-      "week": "lower",
-      "course": "Лекція",
-      "title": "Історія політичного устрою країн Західної Європи ",
-      "teacher": "Іванов О. Ф.",
-      "location": "451 аудиторія",
-      "additionalInfo": ""
+      "week": "all",
+      "course": "",
+      "title": "",
+      "teacher": "",
+      "location": "",
+      "additionalInfo": "",
+    },
     },
     {
       "time": "14:40",
-      "week": "lower",
-      "course": "",
-      "title": "",
-      "teacher": "",
-      "location": "",
-      "additionalInfo": ""
-    },
-    {
-      "time": "16:20",
-      "week": "lower",
-      "course": "",
-      "title": "",
-      "teacher": "",
-      "location": "",
-      "additionalInfo": ""
-    },
-    {
-      "time": "17:55",
-      "week": "lower",
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": ""
-    },
-    {
-      "time": "13:05",
-      "week": "upper",
-      "course": "",
-      "title": "",
-      "teacher": "",
-      "location": "",
-      "additionalInfo": ""
-    },
-    {
-      "time": "14:40",
-      "week": "upper",
-      "course": "Семінар",
-      "title": "Історія політики країн Заходу щодо України",
-      "teacher": "Машевський О.П.",
-      "location": "460 аудиторія",
-      "additionalInfo": ""
+      "week": "all",
+      "course": "lection",
+      "title": "Джерела до історії Сходу",
+      "teacher": "Урусов В. Б.",
+      "location": "440 аудиторія",
+      "additionalInfo": "",
     },
     {
       "time": "16:20",
       "week": "upper",
-      "course": "Семінар",
-      "title": "Новітня історія України",
-      "teacher": "Шкабко С. І.",
-      "location": "452 аудиторія",
-      "additionalInfo": ""
+      "course": "seminar",
+      "title": "Джерела до історії Сходу",
+      "teacher": "Люлька В. А.",
+      "location": "440 аудиторія",
+      "additionalInfo": "",
     },
-    {
-      "time": "17:55",
-      "week": "upper",
-      "course": "Мова",
-      "title": "Англійська / Французька / Німецька / Іспанська",
-      "teacher": "Шевченко О.Ф / Рижова І.А. / Симканич О.В. / Невинна Ю.П.",
-      "location": "153 / 456 / Онлайн / 452 ауд.",
-      "additionalInfo": ""
-    },
-    // Other lesson objects for different time slots and weeks
   ],
   "friday": [
     {
       "time": "13:05",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
+      "week": "all",
       "course": "",
       "title": "",
       "teacher": "",
       "location": "",
-      "additionalInfo": ""
+      "additionalInfo": "",
+      "alternative": false
     },
     {
       "time": "14:40",
-      "week": "all", // Specify which weeks this lesson applies to (all, upper, or lower)
-      "course": "Лекція",
+      "week": "all",
+      "course": "lection",
       "title": "Дисципліна вільного вибору",
       "teacher": "",
       "location": "Онлайн",
-      "additionalInfo": "https://telegra.ph/Discipl%D1%96na-v%D1%96lnogo-viboru-studenta-03-08"
+      "additionalInfo": "https://telegra.ph/Discipl%D1%96na-v%D1%96lnogo-viboru-studenta-03-08",
+      "alternative": false
     },
-    // Other lesson objects for different time slots and weeks
+    //
+    {
+      "time": "13:05",
+      "week": "all",
+      "course": "",
+      "title": "",
+      "teacher": "",
+      "location": "",
+      "additionalInfo": "",
+      "alternative": true
+    },
+    {
+      "time": "14:40",
+      "week": "all",
+      "course": "lection",
+      "title": "Дисципліна вільного вибору",
+      "teacher": "",
+      "location": "Онлайн",
+      "additionalInfo": "https://telegra.ph/Discipl%D1%96na-v%D1%96lnogo-viboru-studenta-03-08",
+      "alternative": true
+    },
+
   ],
-  // Schedule data for other days
+
+  };
+
+const parameterIcons = {
+  "teacher": "<i class='fas fa-user-tie'></i>&nbsp;",
+  "location": "<i class='fas fa-map-marker-alt'></i>&nbsp;"
 };
+
+const courseNames = {
+  "lection": "Лекція",
+  "seminar": "Семінар",
+  "language": "Мова"
+};
+
 const days = document.querySelectorAll('.day');
 const scheduleContent = document.querySelectorAll('.schedule-content');
 const today = new Date().getDay();
 let currentDay = '';
 let currentWeek = 'upper';
+let isAlternativeMode = false;
+const lessonTimes = ["13:05", "14:40", "16:20", "17:55"];
+
 function displaySchedule(day) {
-  scheduleContent.forEach((content, index) => {
-    content.innerHTML = ''; // Clear schedule content for each time slot
+  scheduleContent.forEach(content => {
+    content.innerHTML = '';
   });
   const dayData = scheduleData[day];
   if (dayData) {
-    if (day === 'thursday' && currentWeek === 'upper') {
-      const dayUpperData = dayData.filter(slot => slot.week === 'upper');
-      if (dayUpperData.length > 0) {
-        dayUpperData.forEach((timeSlotData, index) => {
+    dayData.forEach(timeSlotData => {
+      if ((day !== 'sunday' && day !== 'saturday') && ((timeSlotData.week === 'all' || timeSlotData.week === currentWeek) && 
+          ((!isAlternativeMode && !timeSlotData.alternative) || (isAlternativeMode && timeSlotData.alternative)))) {
+        if (timeSlotData.course) {
           const classElement = document.createElement('div');
           classElement.classList.add('class');
-          classElement.style.float = 'left';
           const courseTitle = document.createElement('strong');
-          courseTitle.textContent = timeSlotData.course;
+          courseTitle.textContent = courseNames[timeSlotData.course];
+          courseTitle.dataset.course = timeSlotData.course;
           classElement.appendChild(courseTitle);
           const title = document.createElement('p');
-          title.textContent = timeSlotData.title;
+          const titleText = document.createElement('span');
+          titleText.textContent = timeSlotData.title;
+          title.appendChild(titleText);
+          titleText.style.fontWeight = 'bold';
           classElement.appendChild(title);
-          const teacher = document.createElement('p');
-          teacher.textContent = `${timeSlotData.teacher}`;
-          classElement.appendChild(teacher);
-          const br = document.createElement('br');
-          classElement.appendChild(br);
-          const location = document.createElement('p');
-          location.textContent = `${timeSlotData.location}`;
-          classElement.appendChild(location);
-          const additionalInfo = document.createElement('p');
-          if (timeSlotData.additionalInfo.includes('https')) {
-            const a = document.createElement('a');
-            a.href = timeSlotData.additionalInfo;
-            a.textContent = timeSlotData.additionalInfo;
-            additionalInfo.appendChild(a);
-          } else {
-            additionalInfo.textContent = `${timeSlotData.additionalInfo}`;
-          }
-          classElement.appendChild(additionalInfo);
-          scheduleContent[index].appendChild(classElement);
-        });
-      } else {
-        scheduleContent.forEach(content => content.innerHTML = '');
-      }
-    } else {
-      dayData.forEach((timeSlotData, index) => {
-        const appliesToWeek = timeSlotData.week === 'all' || timeSlotData.week === currentWeek;
-        if (appliesToWeek) {
-          if (timeSlotData.course) {
-            const classElement = document.createElement('div');
-            classElement.classList.add('class');
-            classElement.style.float = 'left';
-            const courseTitle = document.createElement('strong');
-            courseTitle.textContent = timeSlotData.course;
-            classElement.appendChild(courseTitle);
-            const title = document.createElement('p');
-            title.textContent = timeSlotData.title;
-            classElement.appendChild(title);
-            const teacher = document.createElement('p');
-            teacher.textContent = `${timeSlotData.teacher}`;
-            classElement.appendChild(teacher);
-            const br = document.createElement('br');
-            classElement.appendChild(br);
-            const location = document.createElement('p');
-            location.textContent = `${timeSlotData.location}`;
-            classElement.appendChild(location);
-            const additionalInfo = document.createElement('p');
-            if (timeSlotData.additionalInfo.includes('https')) {
-              const a = document.createElement('a');
-              a.href = timeSlotData.additionalInfo;
-              a.textContent = timeSlotData.additionalInfo;
-              additionalInfo.appendChild(a);
-            } else {
-              additionalInfo.textContent = `${timeSlotData.additionalInfo}`;
+
+          Object.keys(parameterIcons).forEach(parameter => {
+            if (timeSlotData[parameter]) {
+              const icon = document.createElement('span');
+              icon.innerHTML = parameterIcons[parameter];
+              const parameterValue = document.createElement('span');
+              parameterValue.textContent = timeSlotData[parameter];
+              const parameterElement = document.createElement('p');
+              parameterElement.appendChild(icon);
+              parameterElement.appendChild(parameterValue);
+              classElement.appendChild(parameterElement);
             }
+          });
+
+          if (timeSlotData.additionalInfo && timeSlotData.additionalInfo.includes('https')) {
+            const additionalInfo = document.createElement('button');
+            additionalInfo.textContent = 'Посилання';
+            additionalInfo.classList.add('additional-info-button');
+            additionalInfo.addEventListener('click', () => window.open(timeSlotData.additionalInfo, '_blank'));
             classElement.appendChild(additionalInfo);
-            scheduleContent[index].appendChild(classElement);
-          } 
+          }
+          scheduleContent.forEach((content, index) => {
+            if (lessonTimes[index] === timeSlotData.time) {
+              content.appendChild(classElement);
+            }
+          });
         }
-      });
-    }
-  } 
+      }
+    });
+  }
 }
+
 days.forEach(day => {
   day.addEventListener('click', () => {
     days.forEach(d => d.classList.remove('active'));
@@ -308,6 +271,7 @@ days.forEach(day => {
     displaySchedule(currentDay);
   });
 });
+
 document.getElementById('upper-week').addEventListener('click', () => {
   currentWeek = 'upper';
   scheduleContent.forEach(content => content.innerHTML = '');
@@ -315,6 +279,7 @@ document.getElementById('upper-week').addEventListener('click', () => {
   document.getElementById('upper-week').classList.add('active');
   document.getElementById('lower-week').classList.remove('active');
 });
+
 document.getElementById('lower-week').addEventListener('click', () => {
   currentWeek = 'lower';
   scheduleContent.forEach(content => content.innerHTML = '');
@@ -322,9 +287,62 @@ document.getElementById('lower-week').addEventListener('click', () => {
   document.getElementById('lower-week').classList.add('active');
   document.getElementById('upper-week').classList.remove('active');
 });
-if (today !== 0) {
+
+document.getElementById('normal-mode').addEventListener('click', () => {
+  if (isAlternativeMode) {
+    isAlternativeMode = false;
+    displaySchedule(currentDay);
+    document.getElementById('normal-mode').classList.add('active');
+    document.getElementById('alternative-mode').classList.remove('active');
+  }
+});
+
+document.getElementById('alternative-mode').addEventListener('click', () => {
+  if (!isAlternativeMode) {
+    isAlternativeMode = true;
+    displaySchedule(currentDay);
+    document.getElementById('alternative-mode').classList.add('active');
+    document.getElementById('normal-mode').classList.remove('active');
+  }
+});
+
+const resetActiveDay = () => {
+  days.forEach(d => d.classList.remove('active'));
+};
+
+const upperWeekButton = document.getElementById('upper-week');
+const lowerWeekButton = document.getElementById('lower-week');
+
+upperWeekButton.addEventListener('click', () => {
+  if (currentWeek !== 'upper') {
+    currentWeek = 'upper';
+    scheduleContent.forEach(content => content.innerHTML = '');
+    displaySchedule(currentDay);
+    upperWeekButton.classList.add('active');
+    lowerWeekButton.classList.remove('active');
+  }
+});
+
+lowerWeekButton.addEventListener('click', () => {
+  if (currentWeek !== 'lower') {
+    currentWeek = 'lower';
+    scheduleContent.forEach(content => content.innerHTML = '');
+    displaySchedule(currentDay);
+    lowerWeekButton.classList.add('active');
+    upperWeekButton.classList.remove('active');
+  }
+});
+
+if (today === 0) {
+  const currentDayElement = days[6];
+  resetActiveDay();
+  currentDayElement.classList.add('active');
+  currentDay = currentDayElement.dataset.day;
+  displaySchedule(currentDay);
+} else {
   const currentDayElement = days[today - 1];
-  currentDayElement.classList.add('active-today');
+  resetActiveDay();
+  currentDayElement.classList.add('active');
   currentDay = currentDayElement.dataset.day;
   displaySchedule(currentDay);
 }
